d57a3ff2bbcc2389857aea3e69d701c252eb05a8
Brian Mitchell <bman4789@gmail.com>
2016-07-23 15:20:03 -0500

    Styling and move regular expression to regexp.js

--- app.js
+++ app.js
@@ -1,28 +1,29 @@
 var bodyParser = require('body-parser');
 var express = require('express');
+var regexp = require('./regexp');
 var app = express();
 var router = express.Router();
 
 
-var jsonParser = bodyParser.json() 
-var urlencodedParser = bodyParser.urlencoded({ extended: true })
+var jsonParser = bodyParser.json();
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/bae', urlencodedParser, function (req, res) {
-  res.send(req.query.input.replace(/(?!bae|[0-9])[A-GJ-NP-TV-gj-np-tv-z][a][y]?/g, "bae"))
+    res.send(req.query.input.replace(regexp.regexp(), "bae"))
 });
 
 app.get('/bae/:text', urlencodedParser, function (req, res) {
-  res.send(req.params.text)
+    res.send(req.params.text)
 });
 
 app.get('/bae/:text/:more', urlencodedParser, function (req, res) {
-  res.send(req.params.text + "   ---    " + req.params.more)
+    res.send(req.params.text + "   ---    " + req.params.more)
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 3000!');
 });
---
+++ regexp.js
@@ -0,0 +1,6 @@
+var regexp = function() {
+    var exp = "(?!bae|[0-9])[A-GJ-NP-TV-gj-np-tv-z][a][y]?";
+    return new RegExp(exp, "g");
+};
+
+module.exports  = {regexp};
