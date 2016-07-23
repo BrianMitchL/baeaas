592e13807a4a3441e30718ac96a894a2f2732a37
ICSandy <pete9443@morris.umn.edu>
2016-07-23 13:57:38 -0500

    can recieve req, and can res with bae-ified text

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+var router = express.Router();
 
 
-app.use(express.static('public'));
+var jsonParser = bodyParser.json() 
+var urlencodedParser = bodyParser.urlencoded({ extended: true })
 
 
-app.get('/', function (req, res) {
-  res.render(index)
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/bae', urlencodedParser, function (req, res) {
+  res.send(req.query.input.replace(/(?!bae|[0-9])[A-GJ-NP-TV-gj-np-tv-z][a][y]?/g, "bae"))
 });
 
+app.get('/bae/:text', urlencodedParser, function (req, res) {
+  res.send(req.params.text)
+});
+
+app.get('/bae/:text/:more', urlencodedParser, function (req, res) {
+  res.send(req.params.text + "   ---    " + req.params.more)
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
