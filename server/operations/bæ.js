9aecae83ffed838039da6f08edbde7355d2c64aa
Brian Mitchell <bman4789@gmail.com>
2016-07-25 00:11:23 -0500

    Add bæ (this may perform abnormally with the æ character)

---
+++ server/operations/bæ.js
@@ -0,0 +1,18 @@
+regexp = require('../regexp');
+
+module.exports = {
+    name: "BÆ",
+    url: '/bæ/:text',
+    fields: [
+        {
+            name: 'Text',
+            field: 'text'
+        }
+    ],
+    register: function(app, output) {
+        return app.get('/b%C3%A6/:text', function(req, res) {
+            var message = req.params.text.replace(regexp.regexp(), "bæ");
+            return output(req, res, message);
+        });
+    }
+};
