cc61a2c4dd70cc879416b4fdd4e27f7b8ea746fd
Brian Mitchell <bman4789@gmail.com>
2016-07-25 00:11:39 -0500

    Cleanup

--- server/operations/bae.js
+++ server/operations/bae.js
@@ -11,8 +11,7 @@ module.exports = {
     ],
     register: function(app, output) {
         return app.get('/bae/:text', function(req, res) {
-            var message;
-            message = req.params.text.replace(regexp.regexp(), "bae");
+            var message = req.params.text.replace(regexp.regexp(), "bae");
             return output(req, res, message);
         });
     }
